a0494a0a43b8f1d42b7178e66de4d0d8787a3759
HBHATI04 <117622958+HBHATI04@users.noreply.github.com>
2024-12-02 22:04:44 +0530

    Update OrderSummary.jsx

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -6,7 +6,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import axios from "../lib/axios";
 
 const stripePromise = loadStripe(
-	"pk_test_51KZYccCoOZF2UhtOwdXQl3vcizup20zqKqT9hVUIsVzsdBrhqbUI2fE0ZdEVLdZfeHjeyFXtqaNsyCJCmZWnjNZa00PzMAjlcL"
+	"pk_test_51QRZkd2LG1kwqgzomnJZhjDX1s4O1jfiZW5Omh4IIxqvFzTyxRRspJdnwF66DwRwzrF6PDCc5hmmZdSn9wL3Sdrm00nqDa3meW"
 );
 
 const OrderSummary = () => {
